aba18584a32b6d4dffdf97d629ed1261ea421f52
Dony Indiarto <dony.indiarto@gmail.com>
2023-04-19 22:17:36 +0700

    added a small yet functional test script

--- tests/testthat/test-vis_map_temperature.R
+++ tests/testthat/test-vis_map_temperature.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(testthat)
+
+# Test if function returns a ggplot object
+test_that("vis_map_temperature returns a ggplot object", {
+  ntt_temp_path <- LUMENSR_example("ntt_temp.tif")
+  ntt_temp <-  terra::rast(ntt_temp_path)
+  plot <- vis_map_temperature(temperature_data = ntt_temp, boundary_data = ntt_admin)
+  expect_true(inherits(plot, "ggplot"))
 })
