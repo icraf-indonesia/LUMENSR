bf7d1c8e581f7f5ff1e5b88244f0ac775d8643dc
Dony Indiarto <dony.indiarto@gmail.com>
2023-07-12 11:39:12 +0700

    Update add_legend_to_categorical_raster.R
    In this commit, the add_legend_to_categorical_raster function is updated to include guardrails that check the validity of input parameters. The function now ensures that raster_file is a SpatRaster object and lookup_table is a dataframe. It also checks whether the first column of lookup_table is numeric or can be converted to numeric. If the first column is not numeric, it is converted within the function. Additionally, the function has been refactored to use base R subsetting operations instead of dplyr pipes for better compatibility and performance.

--- R/add_legend_to_categorical_raster.R
+++ R/add_legend_to_categorical_raster.R
@@ -12,10 +12,43 @@
 #' \dontrun{
 #' add_legend_to_categorical_raster(raster_file = kalbar_11, lookup_table = lc_lookup_klhk) %>% plot()
 #' }
-add_legend_to_categorical_raster <- function(raster_file, lookup_table){
-  name_rast <-names(raster_file)
+add_legend_to_categorical_raster <- function(raster_file, lookup_table) {
+  # Check if raster_file is a SpatRaster object
+  if (!inherits(raster_file, "SpatRaster")) {
+    stop("raster_file should be a SpatRaster object")
+  }
 
+  # Check if lookup_table is a dataframe
+  if (!is.data.frame(lookup_table)) {
+    stop("lookup_table should be a dataframe")
+  }
+
+  # Check if the first column of lookup_table is numeric or convertible to numeric
+  first_column <- lookup_table[[1]]
+  if (!is.numeric(first_column) && any(is.na(as.numeric(first_column)))) {
+    stop("The first column of lookup_table should be numeric or convertible to numeric")
+  }
+
+  # Filter lookup_table to only include values present in raster_file
+  lookup_table <- lookup_table[lookup_table$Value %in% terra::freq(raster_file)[["value"]],]
+
+  # Convert lookup_table into a dataframe
+  lookup_table <- data.frame(lookup_table)
+
+  # Convert the first column to numeric if it is not already
+  if (!is.numeric(first_column)) {
+    lookup_table[[1]] <- as.numeric(first_column)
+  }
+
+  # Get the names of raster_file
+  name_rast <- names(raster_file)
+
+  # Set the levels of raster_file to be lookup_table
   levels(raster_file) <- lookup_table
-  raster_file <- setNames(raster_file,name_rast)
+
+  # Set the names of raster_file
+  raster_file <- setNames(raster_file, name_rast)
+
+  # Return the modified raster_file
   return(raster_file)
 }
