29668608f8b2b4c812016dfdb4a08aa6b0b9e040
Dony Indiarto <dony.indiarto@gmail.com>
2023-08-01 15:30:02 +0700

    Create reclass_raster_to_categories.R
    towards #19

---
+++ R/reclass_raster_to_categories.R
@@ -0,0 +1,35 @@
+#' Reclassify a Raster to Categories
+#'
+#' This function reclassifies a raster using a provided reclassification table.
+#'
+#' @param raster_map A raster object to be reclassified.
+#' @param reclass_table A data frame containing the reclassification rules. The first column must be the existing
+#' values (ID_check) and the second column should be the new values (reclass_mat).
+#'
+#' @return
+#' A reclassified factor raster.
+#'
+#' @examples
+#' \dontrun{
+#' # assuming we have a raster object "my_raster" and a reclassification table "my_table"
+#' my_reclassed_raster <- reclass_raster_to_categories(my_raster, my_table)
+#' }
+#'
+#' @export
+#' @importFrom terra classify droplevels as.factor cats
+#' @importFrom dplyr select_if rename left_join select
+#' @importFrom purrr pluck
+#'
+reclass_raster_to_categories <- function(raster_map, reclass_table){
+  # Retrieve unique IDs from the raster and rename column to 'Value'
+  ID_check <- terra::droplevels(lc_t1_) |> terra::cats() |> pluck(1) |> select(1)
+
+  # Join the reclassification table with the ID_check,
+  # keep only the numeric columns and convert to matrix
+  reclass_mat <- left_join(ID_check, reclass_table, by="Value") %>% select_if(is.numeric) %>% as.matrix()
+
+  # Reclassify the raster using the reclassification matrix and convert to factor
+  raster_map_reclass <- classify(raster_map, reclass_mat) %>% as.factor()
+
+  return(raster_map_reclass)
+}
